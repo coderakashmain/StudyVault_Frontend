be325e5824499d9e19f6471b5205626c672054bc
Jitu Pradhan <jitupra73@gmail.com>
2024-07-10 23:25:13 +0530

    add

--- src/Component/Home-T/HomeT.jsx
+++ src/Component/Home-T/HomeT.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import './Home.css'
+
 const HomeT = () => {
   return (
     <>
-      {/* <div className="main-container">
+      <div className="main-container">
         <div className="inner-main-container">
           <div>
-            <div><input type="text" name="" id="" /></div>
-            <span></span>
+            <div><input type="text" name="" id="" placeholder="Type here" /></div>
           </div>
         </div>
-      </div> */}
-      
+      </div>
+     
+    
     </>
   );
 };
