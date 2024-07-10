cbd0e183a7f31ed37d3300534279d09728013820
Jitu Pradhan <jitupra73@gmail.com>
2024-07-10 22:22:23 +0530

    login added

--- src/Component/Home-T/HomeT.jsx
+++ src/Component/Home-T/HomeT.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-
+import React from "react";
+import Login from "./Login/Login";
 const HomeT = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      {/* <div className="main-container">
+        <div className="inner-main-container">
+          <div>
+            <div><input type="text" name="" id="" /></div>
+            <span></span>
+          </div>
+        </div>
+      </div> */}
+      <Login/>
+    </>
+  );
+};
 
-export default HomeT
+export default HomeT;
