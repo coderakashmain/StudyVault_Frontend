060fbe3f81b99e502c1e7b306c792b45fb549276
Akash Bindhani <ab791235@gmail.com>
2024-07-11 15:46:42 +0530

    add profile

--- src/Component/Home-T/Home-P/Profile/Profile.jsx
+++ src/Component/Home-T/Home-P/Profile/Profile.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import './Profile.css'
 
 const Profile = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="profile">
+      <div className="profile-outer-box">
+        <div className="profile-info">
+          <div className="profile-photoes">
+          <div className="profile-cover-photo">
+             {/* <img src="https://images.pexels.com/photos/1045535/pexels-photo-1045535.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /> */}
+          </div>
+          <div className="profile-main-photo">
+            {/* <img src="https://images.pexels.com/photos/2027394/pexels-photo-2027394.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
--- src/Component/Home-T/HomeT.jsx
+++ src/Component/Home-T/HomeT.jsx
@@ -2,19 +2,13 @@
 
 import React from "react";
 import "./Home.css";
+import Profile from "./Home-P/Profile/Profile";
+
 
 const HomeT = () => {
   return (
     <>
-      <div className="main-container">
-        <div className="inner-main-container">
-          <div>
-            <div>
-              <input type="text" name="" id="" placeholder="Type here" />
-            </div>
-          </div>
-        </div>
-      </div>
+      <Profile/>
     </>
   );
 };
