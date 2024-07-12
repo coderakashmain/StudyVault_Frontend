10646b6392147826ba1ca651e4acb27d448bce71
Akash Bindhani <ab791235@gmail.com>
2024-07-12 21:41:48 +0530

    profile

--- src/Component/Home-T/Home-P/Profile/Profile.jsx
+++ src/Component/Home-T/Home-P/Profile/Profile.jsx
@@ -6,17 +6,13 @@ const Profile = () => {
     <section id="profile">
       <div className="profile-outer-box">
         <div className="profile-left-inner-box">
-        
-         
           <div className="profile-info">
-          <div className="upperstyle">
-            
-            </div>
+            <div className="upperstyle"></div>
             <div className="profile-photoes">
               <div className="profile-main-photo-box">
                 <h2>Jotyshree Lohar</h2>
                 <h3>
-                  <i class="fa-solid fa-location-dot"></i>MPC AUTO, BARIPADA
+                  <i className="fa-solid fa-location-dot"></i>MPC AUTO, BARIPADA
                 </h3>
                 <div className="profile-main-photo">
                   <div className="profile-main-photo-subpart"></div>
@@ -33,71 +29,79 @@ const Profile = () => {
                 </div>
               </div>
               <div className="pdfupload">
-                 
-                <button>Upload Now</button>
+                <button>
+                  Upload Now
+                  <i className="fa-solid fa-arrow-up-from-bracket"></i>
+                </button>
               </div>
-              <p style={{textAlign:'center',paddingTop:'1.2rem',fontSize:'0.7rem',letterSpacing : '0.1rem',fontWeight:'800'}}>ACHE SE PADHO, BACK MAT LAGNE DENA</p>
-              
+              <p
+                style={{
+                  textAlign: "center",
+                  paddingTop: "1.2rem",
+                  fontSize: "0.7rem",
+                  letterSpacing: "0.1rem",
+                  fontWeight: "800",
+                }}
+              >
+                ACHE SE PADHO, BACK MAT LAGNE DENA
+              </p>
             </div>
             <div className="recent-work profilecontain">
               <h3>Recent work</h3>
               <div className="recent-work-box">
-                
-              
-              <div className="recent-download recent">
-                
-              </div>
-              <div className="recent-upload recent">
-
-              </div>
+                <div className="recent-download recent"></div>
+                <div className="recent-upload recent"></div>
               </div>
             </div>
             <div className="profile-client-info profilecontain">
               <h3>Personal Information</h3>
               <div className="forwrap">
-
-                  <div className="client-info-name name">
-                    <h4>Name</h4>
-                    <div className="client-info-name-inside name-insde">
-                      <p>Jotyshree Lohar</p>
-                      <h5>
-                        Name<i class="fa-solid fa-arrow-right"></i>
-                      </h5>
-                    </div>
+                <div className="client-info-name name">
+                  <h4>Name</h4>
+                  <div className="client-info-name-inside name-insde">
+                    <p>Jotyshree Lohar</p>
+                    <h5>
+                      Name<i className="fa-solid fa-arrow-right"></i>
+                    </h5>
                   </div>
-                  <div className="client-info-name department">
-                    <h4>Department</h4>
-                    <div className="client-info-name-inside name-insde">
-                      <p>Computer Science</p>
-                      <h5>
-                        Department<i class="fa-solid fa-arrow-right"></i>
-                      </h5>
-                    </div>
+                </div>
+                <div className="client-info-name department">
+                  <h4>Department</h4>
+                  <div className="client-info-name-inside name-insde">
+                    <p>Computer Science</p>
+                    <h5>
+                      Department<i className="fa-solid fa-arrow-right"></i>
+                    </h5>
                   </div>
-                  </div>
-                  <div className="forwrap">
-                  <div className="client-info-name gmail">
-                    <h4>Gmail</h4>
-                    <div className="client-info-name-inside name-insde">
-                      <p>jotyshree@gmail.com</p>
-                      <h5>
-                        Gmail<i class="fa-solid fa-arrow-right"></i>
-                      </h5>
-                    </div>
-                  </div>
-                  <div className="client-info-name rollno">
-                <h4> Roll No</h4>
-                <div className="client-info-name-inside name-insde">
-                  <p>BT42-843</p>
-                  <h5>
-                   Roll No<i class="fa-solid fa-arrow-right"></i>
-                  </h5>
                 </div>
               </div>
-             </div>
+              <div className="forwrap">
+                <div className="client-info-name gmail">
+                  <h4>Gmail</h4>
+                  <div className="client-info-name-inside name-insde">
+                    <p>jotyshree@gmail.com</p>
+                    <h5>
+                      Gmail<i className="fa-solid fa-arrow-right"></i>
+                    </h5>
+                  </div>
+                </div>
+                <div className="client-info-name rollno">
+                  <h4> Roll No</h4>
+                  <div className="client-info-name-inside name-insde">
+                    <p>BT42-843</p>
+                    <h5>
+                      Roll No<i className="fa-solid fa-arrow-right"></i>
+                    </h5>
+                  </div>
+                </div>
+              </div>
               <div className="edit">
-               <button>Log out<i class="fa-solid fa-arrow-right-from-bracket"></i></button>
-                <button>Edit profile<i class="fa-solid fa-pen-to-square"></i>
+                <button>
+                  Log out
+                  <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                </button>
+                <button>
+                  Edit profile<i className="fa-solid fa-pen-to-square"></i>
                 </button>
               </div>
             </div>
@@ -105,32 +109,82 @@ const Profile = () => {
         </div>
 
         <div className="profile-right-inner-box">
-        <div className="latest-updates">
-             <h2>Latest Updates</h2>
-             <div className="latest-updates-files">
-              <h3>Now available</h3>
-              <div className="files-items">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in.🍫</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in.🍫</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid officia in🍫.</p>
-
-              </div>
-             </div>
+              <div className="latest-updates">
+                <h2>Latest Updates</h2>
+                <div className="latest-updates-files">
+                  <h3>Now available</h3>
+                  <div className="files-items">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in.🍫
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in.🍫
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Aliquid officia in🍫.
+                    </p>
+                  </div>
+                </div>
+            <div className="query-box">
+              <img src="" alt="" />
+              <h2>
+                If You have any Query or arise any problemn then You can be contact with us .
+              </h2>
+                <p>We always present to solve your Queris.</p>
+            </div>
           </div>
         </div>
-          
       </div>
     </section>
   );
