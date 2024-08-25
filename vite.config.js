acaa58a5c177205e76376a51044e829ea82dd66a
Akash Bindhani <ab791235@gmail.com>
2024-08-25 20:46:03 +0530

    modify core

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server : {
     proxy : {
-      '/api': process.env.VITE_API_URL || "http://localhost:3000",
+      '/api': 'https://www.studyvault.online',
     }
   },
   plugins: [react()],
