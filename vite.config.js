ad46c25696b1c8d839affb0b58b1e2d124c31754
Akash Bindhani <ab791235@gmail.com>
2024-11-15 11:03:04 +0530

    lotie

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-
-// console.log('VITE_API_URL:', process.env.VITE_API_URL);
-// https://vitejs.dev/config/
 export default defineConfig({
-  server : {
-    proxy : {
+  server: {
+    proxy: {
       '/api': {
-        target: process.env.VITE_API_URL ||'http://localhost:3000' ,  
-        changeOrigin: true,            
-        secure: process.env.NODE_ENV === 'production'            
-      },                   
-    }
+        target: process.env.VITE_API_URL || 'http://localhost:3000',
+        changeOrigin: true,
+        secure: process.env.NODE_ENV === 'production',
+      },
+    },
   },
   build: {
-    chunkSizeWarningLimit: 1000,  // Increase the limit to 1000 kB
+    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          // Separate lottie-web if needed to reduce build size issues
+          if (id.includes('lottie-web')) {
+            return 'lottie';
+          }
+        },
+      },
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
-  
-
+  define: {
+    'process.env': {}, // Define process.env for modules that expect it
+    'eval': '0', // Replace eval with a harmless value
+  },
   plugins: [react()],
   resolve: {
     alias: {
-      
-    }
-  }
+      'lottie-web': 'lottie-web/build/player/lottie_light', // Use the light version of lottie-web
+    },
+  },
 });
