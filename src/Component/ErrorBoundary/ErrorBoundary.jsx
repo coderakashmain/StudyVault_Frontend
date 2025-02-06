4047eda11a5fcbb9835bc6be870789c370a2f4ed
Akash Bindhani <ab791235@gmail.com>
2025-02-06 09:27:28 +0530

    n

--- src/Component/ErrorBoundary/ErrorBoundary.jsx
+++ src/Component/ErrorBoundary/ErrorBoundary.jsx
@@ -17,7 +17,8 @@ class ErrorBoundary extends React.Component {
   
     render() {
       if (this.state.hasError) {
-        return <div style={{position : 'fixed',zIndex : '1111111111111111',background : 'white', left : '0', top : '0', display : 'flex', justifyContent : 'center', alignItems : 'center',height :'100%', width : '100%'}}> <h1 style={{margin : '2rem', textAlign : 'center'}}>Something went wrong. <br /> <span style={{fontSize : '1.4rem'}}>Please check the url or Refresh the page again.</span></h1></div>;
+        return <div style={{position : 'fixed',zIndex : '1111111111111111',background : 'white', left : '0', top : '0', display : 'flex', justifyContent : 'center', alignItems : 'center',height :'100%', width : '100%'}}> <h1 style={{margin : '2rem', textAlign : 'center'}}>We are Working on somthing.<br /> Back soon. <span style={{fontSize : '1.4rem'}}>
+          </span></h1></div>;
       }
   
       return this.props.children;
